44a34dab62ef71c64e16cf1dcb24720e21fbdee1
098orin <098orin0102@gmail.com>
2025-08-10 11:19:46 +0900

    b64_hex.js v1

---
+++ b64_hex.js
@@ -0,0 +1,34 @@
+class b64 {
+    getInfo() {
+        return {
+            id: "b64hex",
+            name: "base64 to hex",
+            blocks: [
+                {
+                    opcode: 'b64ToHex',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'Decode [DATA] as hex',
+                    arguments: {
+                        DATA: {
+                            type: Scratch.ArgumentType.STRING
+                        }
+                    }
+                }
+            ]
+        }
+    }
+
+    b64ToHex(args) {
+        base64 = args.DATA.toString()
+        // Decode Base64 to binary string
+        const binaryString = atob(base64);
+
+        // Convert binary string to hexadecimal
+        return Array.from(binaryString)
+            .map(char => char.charCodeAt(0).toString(16).padStart(2, '0'))
+            .join('');
+
+    }
+}
+
+Scratch.extensions.register(new b64());
